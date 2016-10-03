cd0d1fae0256989f7523f0ed1d99edb63b6934cf
acev <aasisvinayak@gmail.com>
2016-10-03 16:38:12 +0100

    initial commit

---
+++ PromoCode.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Flymyshop\Plugins\PromoCode;
+
+use Flymyshop\Plugins\Plugin;
+
+/**
+ * Class PromoCode
+ * @package Flymyshop\Plugins\PromoCode
+ */
+class PromoCode  implements Plugin
+{
+         /**
+         * Main class of the plugin which is invoked by the reflector class
+         *
+         */
+        public static function main()
+        {
+        }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fms_promo_code_plugin
+# Promo code plugin for Fly My Shop
 Plugin for FlyMyShop that adds the ability for the shop admin to add promo codes
